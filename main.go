31db90af48f9a91c1a9c3d29e4f4aa1072948e1e
Mike Webster <email@mikewebster.tech>
2021-05-06 20:19:22 -0500

    add todos

--- main.go
+++ main.go
@@ -4,6 +4,17 @@ import (
     "fmt"
 )
 
+// TODO:
+// - Scrape box score for given day (need runs scored and earned runs for each team)
+// - Save data (csv file)
+// - Read saved data
+// - Iterate through a year of saved data to calculate display data
+//    - The idea is to add a "frame" for each day of the year and
+//      for each frame we update the standings
+// - Find ui tool to display charts
+// - Build ui to navigate through frames
+// - Find images to use for each team
+
 func main() {
     fmt.Println("hello world!")
 }
